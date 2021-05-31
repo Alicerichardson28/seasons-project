4a041d031c9139bb6cb1f3d206e914f35ae47431
Parada <paradafaii28@gmail.com>
2021-05-31 13:15:24 -0400

    add geolocation api

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 
 
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        //need two argument 
+        // call back function, this going to show your current location
+        (position) => console.log(position),
+        //this call back will call anytime the location not found
+        err => console.log(err)
+    );
     return <div>Hi there!</div>;
 };
 
