36b36f4c2e22444afce5d35f10d3465136857a2c
Parada <paradafaii28@gmail.com>
2021-06-05 18:26:48 -0400

    passing props to seasonDisplay component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
 
 
 // const App = () => {
@@ -18,39 +19,40 @@ class App extends React.Component {
     //constructor function going to be auto and instantly called before anything else
     //constructor belong to js, not specific to react
 
-    constructor(props) {
-        //super is reference to the parent constructor function
-        super(props);
+    // constructor(props) {
+    //     //super is reference to the parent constructor function
+    //     super(props);
 
-        this.state = {
-            //we don't know latitude yet, we set that to null
-            //THIS IS THE ONLY TIME we do direct assignment
-            //to this.state
-            lat: null,
-            errorMessage: ''
-        };
+    //     this.state = {
+    //         //we don't know latitude yet, we set that to null
+    //         //THIS IS THE ONLY TIME we do direct assignment
+    //         //to this.state
+    //         lat: null,
+    //         errorMessage: ''
+    //     };
+    // }
 
-        window.navigator.geolocation.getCurrentPosition(
-            //need two argument 
-            // call back function, this going to show your current location
-            (position) => {
-                //anytime you want update state we call setState only, setState is function, come from react Component
-                this.setState({ lat: position.coords.latitude })
-
-                // //we did not
-                // this.state.lat = position.coords.latitude
-            },
-    
-            //this call back will call anytime the location not found or client denied 
-            err => {
-                this.setState({ errorMessage: err.message });
-            }
-        );
+    //same as written constructor
+    //implement to babel and babel going to write constructor be hide the seen for you
+    state = {
+        lat: null,
+        errorMessage: ''
     }
 
     //componentDidMount going to show every time when component first rendered
     componentDidMount() {
-        console.log('My component was rendered to the screen');
+        window.navigator.geolocation.getCurrentPosition(
+            //need two argument 
+            // call back function, this going to show your current location
+            (position) => this.setState({ lat: position.coords.latitude }),
+                //anytime you want update state we call setState only, setState is function, come from react Component
+                // //we did not
+                // this.state.lat = position.coords.latitude
+            
+    
+            //this call back will call anytime the location not found or client denied 
+            err => this.setState({ errorMessage: err.message })
+        );
     }
 
     //componentDidUpdate going to update when the state is update
@@ -64,7 +66,7 @@ class App extends React.Component {
         }
 
         if (!this.state.errorMessage && this.state.lat){
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat}/>
         }
         // when waiting loading 
         return <div>Loading ....</div>
